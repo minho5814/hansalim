7906661ccf7779d3f375d27585b66a478e69c5cd
no01-PC\no01 <tkmino84@gmail.com>
2018-11-05 13:16:57 +0900

    no message

--- mobile/js/common.js
+++ mobile/js/common.js
@@ -255,6 +255,11 @@ $(document).ready(function(){
 });
 
 
+$(window).load(function(){
+	/* 화면 로드시 노출되는 팝업 */
+	$('.loadPopup').show();
+});
+
 $(window).scroll(function(){
 	var winTop = $(window).scrollTop();
 	var headH = $('.header').outerHeight();
